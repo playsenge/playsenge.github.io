5e1c9f808db543a8978050344f69c71dd96fa707
Glitch (glitch-hello-website) <none>
2021-09-30 18:06:27 +0000

    🏆🙏 Checkpoint ./css/main.css:10807862/52 ./index.html:10807862/2121 ./contact.html:10807862/1262 ./contact/index.html:10807862/1498 ./navbar.html:10807862/48 ./js/navbar.js:10807862/101 ./js/selecttab.js:10807862/629

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,9 @@
-function goHome(e) {
-    redirect("/", e);
+function goHome() {
+    redirect("/");
 }
 
-function redirect(site, e){
-    if(!e.target.classList.includes("active")) {
-        window.parent.location.href = site;
-    }
+function redirect(site)
+  window.parent.location.href = site;
 }
 
 // professionalism 1000
