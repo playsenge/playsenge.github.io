16feaea7782b9aa3813f6c08deccec8a3f5d4a4b
Glitch (glitch-hello-website) <none>
2021-10-01 12:37:23 +0000

    🏍🍅 Checkpoint ./bookmarks/index.html:10807862/47513 ./js/browserextensions.js:10807862/3430

--- js/browserextensions.js
+++ js/browserextensions.js
@@ -1,4 +1,4 @@
-function navigateToExtension(edge, opera, chrome, firefox, safari, ie){    
+function navigateToExtension(e, edge, opera, chrome, firefox, safari, ie){    
 	var browser = (function (agent) {
     switch (true) {
         case agent.indexOf("edge") > -1: return "edge";
@@ -12,28 +12,30 @@ function navigateToExtension(edge, opera, chrome, firefox, safari, ie){
     }
 	})(window.navigator.userAgent.toLowerCase());
 	
+  var extension = e.target.innerText;
+  
 	if(browser == "edge"){
-		if(edge == "unsupported") alert("This extension is not available for Microsoft Edge.")
+		if(edge == "unsupported") alert(extension + " is not available for Microsoft Edge.")
 		else window.location.href = edge;
 	}
 	else if(browser == "opera"){
-		if(opera == "unsupported") alert("This extension is not available for Opera.")
+		if(opera == "unsupported") alert(extension + " is not available for Opera.")
 		else window.location.href = opera;
 	}
 	else if(browser == "chrome"){
-		if(chrome == "unsupported") alert("This extension is not available for Google Chrome.")
+		if(chrome == "unsupported") alert(extension + " is not available for Google Chrome.")
 		else window.location.href = chrome;
 	}
 	else if(browser == "ie"){
-		if(ie == "unsupported") alert("This extension is not available for Internet Explorer.")
+		if(ie == "unsupported") alert(extension + " is not available for Internet Explorer.")
 		else window.location.href = ie;
 	}
 	else if(browser == "firefox"){
-		if(firefox == "unsupported") alert("This extension is not available for Firefox.")
+		if(firefox == "unsupported") alert(extension + " is not available for Firefox.")
 		else window.location.href = firefox;
 	}
 	else if(browser == "safari"){
-		if(safari == "unsupported") alert("This extension is not available for Safari.")
+		if(safari == "unsupported") alert(extension + " is not available for Safari.")
 		else window.location.href = safari;
 	} else {
     alert("Error: Can't detect your browser.");
