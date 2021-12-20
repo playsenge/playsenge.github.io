4b28872041a47a5d539e090c8d9d18c78b9a5866
playsenge <40773107+playsenge@users.noreply.github.com>
2021-12-20 10:27:20 +0100

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,8 @@
-function navigate(site, e) {
-  if(!e.target.classList.contains("active")) window.parent.location.href = site;
+function navigate(site, e, newtab) {
+  if(!e.target.classList.contains("active")) {
+    if(newtab == false) window.parent.location.href = site;
+    else window.open(site, "_blank");
+  }
 }
 
 // professionalism 1000
